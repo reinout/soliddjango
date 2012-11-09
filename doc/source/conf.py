75ec28523667a2bf677e1aa57f510272b0d2559f
Reinout van Rees <reinout@vanrees.org>
2012-11-09 09:29:53 +0100

    Fixed project name: should not have a space in it.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -7,7 +7,7 @@
 import datetime
 
 
-project = "Solid Django"
+project = "soliddjango"
 author = "Reinout van Rees"
 version = "0.1"
 release = ""
@@ -95,7 +95,7 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'Solid Django (<release>)'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -170,7 +170,7 @@ latex_font_size = '11pt'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', '%s.tex' % project, u'%s Documentation' % project,
+  ('index', '%s.tex' % project, u'Solid Django',
    author, 'manual'),
 ]
 
