3c74f38e32d1d62808da6c06138710b40b15840c
Reinout van Rees <reinout@vanrees.org>
2012-12-01 22:25:28 +0100

    Fixed intersphinx links to django docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -5,7 +5,8 @@
 # serve to show the default.
 
 import datetime
-
+import os
+import sys
 
 project = "soliddjango"
 author = "Reinout van Rees"
@@ -20,7 +21,8 @@ copyright = '%s, %s' % (this_year, author)
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "_ext")))
 
 # -- General configuration -----------------------------------------------------
 
@@ -29,7 +31,8 @@ copyright = '%s, %s' % (this_year, author)
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.viewcode']
+extensions = ['djangodocs', 'sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
+              'sphinx.ext.todo', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
